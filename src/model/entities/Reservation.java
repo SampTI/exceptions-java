a9ed1cd8937390cd2ac5e1700bbeb918a10d15a2
Tiago Sampaiogit config --global user.name Tiago <Teagol10@gmail.com>
2021-06-22 21:21:01 -0300

    Good solution

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -4,6 +4,7 @@ package model.entities;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 import java.text.SimpleDateFormat;
+import model.exception.DomainException;
 
 public class Reservation {
     private Integer roomNumber;
@@ -12,9 +13,12 @@ public class Reservation {
     
     private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-    public Reservation(Integer roomNumber, Date checkin, Date checkOut) {
+    public Reservation(Integer roomNumber, Date checkIn, Date checkOut)  {
+        if(!checkOut.after(checkIn)){
+                throw new DomainException("Error in reservation: Check-out date must be after check-in date");
+            }
         this.roomNumber = roomNumber;
-        this.checkIn = checkin;
+        this.checkIn = checkIn;
         this.checkOut = checkOut;
     }
 
@@ -39,18 +43,18 @@ public class Reservation {
         long diff = checkOut.getTime()- checkIn.getTime();
         return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
     }
-    public String updateDates(Date checkIn, Date checkOut){
+    public void updateDates(Date checkIn, Date checkOut) {
         Date now = new Date();
             
             if(checkIn.before(now)|| checkOut.before(now)){
-               return "Error in reservation: Reservation dates for update must be future dates";
+               throw new DomainException("Error in reservation: Reservation dates for update must be future dates");
             }
             if(!checkOut.after(checkIn)){
-                return "Error in reservation: Check-out date must be after check-in date";
+                throw new DomainException("Error in reservation: Check-out date must be after check-in date");
             }
         this.checkIn = checkIn;
         this.checkOut = checkOut;
-        return null;
+       
     }
     @Override
     public String toString(){
